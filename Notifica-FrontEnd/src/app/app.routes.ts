2cb7a2fde9f2d738dd23dab18b7c8b621185d956
Beater-IKO <samuelsilva.wly@gmail.com>
2025-09-10 21:29:25 -0300

    salvando alterações antes de atualizar angular

--- Notifica-FrontEnd/src/app/app.routes.ts
+++ Notifica-FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CriacaoTicketsComponent } from './components/criacao-tickets/criacao-tickets.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:"", redirectTo:"criacao-tickets", pathMatch:"full"},
+    {path:"criacao-tickets", component: CriacaoTicketsComponent}
+
+
+];
---
+++ Notifica-FrontEnd/src/app/components/criacao-tickets/criacao-tickets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criacao-tickets',
+  imports: [],
+  templateUrl: './criacao-tickets.component.html',
+  styleUrl: './criacao-tickets.component.scss'
+})
+export class CriacaoTicketsComponent {
+
+}
